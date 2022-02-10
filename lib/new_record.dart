a848e77f2385d21437ac1adb59051f23171363fb
Christopher Ormaza <chris.ormaza@gmail.com>
2022-02-10 16:00:43 -0500

    Added images for day type Added selection with images loaded Added conditional menstrual field

--- lib/new_record.dart
+++ lib/new_record.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class NewEditPageWidget extends StatefulWidget {
   const NewEditPageWidget({Key? key, required this.title}) : super(key: key);
@@ -11,10 +11,38 @@ class NewEditPageWidget extends StatefulWidget {
 
 class NewEditPageState extends State<NewEditPageWidget> {
   DateTime selectedDate = DateTime.now();
+  String? dayTypeDropdownValue;
+  String? menstrualTypeDropdownValue;
+  bool _menstrualShow = false;
   TextEditingController dateCtl = TextEditingController();
   DateFormat formatter = DateFormat.yMMMMd();
   @override
   Widget build(BuildContext context) {
+    Map<String, String> imageLabels = {
+      'peak': AppLocalizations.of(context)!.peak,
+      'peak_1': AppLocalizations.of(context)!.peak_1,
+      'peak_2': AppLocalizations.of(context)!.peak_2,
+      'peak_3': AppLocalizations.of(context)!.peak_3,
+      'peak_p': AppLocalizations.of(context)!.peak_p,
+      'g_peak': AppLocalizations.of(context)!.g_peak,
+      'g_peak_1': AppLocalizations.of(context)!.g_peak_1,
+      'g_peak_2': AppLocalizations.of(context)!.g_peak_2,
+      'g_peak_3': AppLocalizations.of(context)!.g_peak_3,
+      'y_peak': AppLocalizations.of(context)!.y_peak,
+      'y_peak_1': AppLocalizations.of(context)!.y_peak_1,
+      'y_peak_2': AppLocalizations.of(context)!.y_peak_2,
+      'y_peak_3': AppLocalizations.of(context)!.y_peak_3,
+      'red': AppLocalizations.of(context)!.red,
+      'green': AppLocalizations.of(context)!.green,
+      'yellow': AppLocalizations.of(context)!.yellow,
+    };
+    Map<String, String> menstrualLabels = {
+      "menstrual_h": AppLocalizations.of(context)!.menstrual_h,
+      "menstrual_m": AppLocalizations.of(context)!.menstrual_m,
+      "menstrual_l": AppLocalizations.of(context)!.menstrual_l,
+      "menstrual_vl": AppLocalizations.of(context)!.menstrual_vl,
+      "menstrual_b": AppLocalizations.of(context)!.menstrual_b,
+    };
     return Center(
       child: Form(
           child: Scrollbar(
@@ -41,6 +69,7 @@ class NewEditPageState extends State<NewEditPageWidget> {
                       if (value == null || value.isEmpty) {
                         return 'Choose Date';
                       }
+                      return null;
                     },
                     decoration: const InputDecoration(
                       labelText: 'Record Date.',
@@ -49,7 +78,69 @@ class NewEditPageState extends State<NewEditPageWidget> {
                       labelStyle:
                           TextStyle(decorationStyle: TextDecorationStyle.solid),
                     ),
-                  )
+                  ),
+                  const SizedBox(height: 10),
+                  DropdownButtonHideUnderline(
+                      child: DropdownButton(
+                    icon: const Icon(Icons.arrow_downward),
+                    hint: const Center(
+                      child: Text("Select day type"),
+                    ),
+                    items: imageLabels.keys
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(5),
+                              child: Image.asset(
+                                  'assets/images/' + value + '.png'),
+                            ),
+                            const SizedBox(width: 50),
+                            Text(imageLabels[value]!),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        if (newValue == "red") {
+                          _menstrualShow = true;
+                        } else {
+                          _menstrualShow = false;
+                        }
+                        dayTypeDropdownValue = newValue!;
+                      });
+                    },
+                    isExpanded: true,
+                    value: dayTypeDropdownValue,
+                  )),
+                  const SizedBox(height: 10),
+                  Visibility(
+                      visible: _menstrualShow,
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton(
+                          icon: const Icon(Icons.arrow_downward),
+                          hint: const Center(
+                            child: Text("Select menstrual flow type"),
+                          ),
+                          items: menstrualLabels.keys
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(menstrualLabels[value]!));
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              menstrualTypeDropdownValue = newValue!;
+                            });
+                          },
+                          isExpanded: true,
+                          value: menstrualTypeDropdownValue,
+                        ),
+                      ))
                 ],
               ),
             ),
